2c2758b25f87659e117dee57a41df0337d074f1e
sorabh sharma <ssorabh.ssharma@gmail.com>
2018-04-11 19:48:56 +0530

    first setup

---
+++ README.md
@@ -0,0 +1,44 @@
+eventcalender-symfony
+=====================
+This is a project using symfony, for add, remove, manage events based on categories.
+
+objectives
+==========
+I am trying to achieved best cut edge understanding of symfony framework. Trying to figure out, how easy to develop a website or webservices using framework. In which case this is best for use.
+
+## Framworks
+* [Symfony 3.4](https://symfony.com/)   
+
+#### Install
+```bash
+$ php -r "file_put_contents('symfony', file_get_contents('https://symfony.com/installer'));"
+```
+#### Create project
+```bash
+$ php symfony new eventcalender-symfony 3.4
+```
+<p style="text-align:center">OR</p>   
+
+```bash
+$ composer create-project symfony/skeleton my_project_name
+```
+
+## 
+
+* bootstrap v4.1.0
+install
+```bash
+$ composer require twbs/bootstrap:4.1.0
+```
+uninstall
+```bash
+$ composer remove twbs/bootstrap
+```
+Composer list commands
+----------------------
+```bash
+$ composer list
+```
+
+
+
---
+++ src/AppBundle/Controller/CategoryController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\BrowserKit\Response;
+
+class CategoryController extends Controller {
+    /**
+     * @Route("/categories", name="category_list")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('category/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/category/create", name="category_create")
+     */
+    public function createAction(Request $request) {
+        // replace this example code with whatever you need
+        return $this->render('category/create.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/category/edit/{id}", name="category_edit")
+     */
+    public function editAction(Request $request) {
+        // replace this example code with whatever you need
+        return $this->render('category/edit.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/category/delete/{id}", name="category_delete")
+     */
+    public function deleteAction(Request $request) {
+        $content = array();
+        return new Response($content);
+    }
+}
