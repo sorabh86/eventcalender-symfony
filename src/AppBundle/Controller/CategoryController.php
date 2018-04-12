29199e49008b3a6a11feb0f95bcfe21e61aa090d
sorabh sharma <ssorabh.ssharma@gmail.com>
2018-04-12 13:27:57 +0530

    create view to add category

--- src/AppBundle/Controller/CategoryController.php
+++ src/AppBundle/Controller/CategoryController.php
@@ -6,7 +6,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\BrowserKit\Response;
-use AppBundle\Entity\Cateogry;
+use AppBundle\Entity\Category;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class CategoryController extends Controller {
     /**
@@ -29,9 +31,42 @@ class CategoryController extends Controller {
      * @Route("/category/create", name="category_create")
      */
     public function createAction(Request $request) {
-        // replace this example code with whatever you need
+        $category = new Category;
+
+        $form = $this->createFormBuilder($category)
+            ->add('name', TextType::class, array('attr' => array('class' => 'form-control', 'style' => 'margin-bottom:15px')))
+            ->add('save', SubmitType::class, array('label' => 'Create Category', 'attr' => array('class' => 'btn btn-secondary')))
+            ->getForm();
+
+        // handle request
+        $form->handleRequest($request);
+
+        // Check Submit
+        if($form->isSubmitted() && $form->isValid()) {
+            $name = $form['name']->getData();
+            // die($name);
+
+            // get current Date and time
+            $now = new \DateTime('now');
+
+            $category->setName($name);
+            $category->setCreateDate($now);
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($category);
+            $em->flush();
+
+            $this->addFlash(
+                'notice',
+                'Category Saved'
+            );
+
+            return $this->redirectToRoute('category_list');
+        }
+
+        // render Template
         return $this->render('category/create.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'form' => $form->createView()
         ]);
     }
 
